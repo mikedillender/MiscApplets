61c3bb159e80d81d5022c73606aac66e07d53a55
CuseCose <dillemic000@gmail.com>
2018-09-20 16:20:02 -0500

    tetris

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,8 @@ public class Main extends Applet implements Runnable, KeyListener {
 
     //COLORS
     Color background=new Color(255, 255, 255);
-    Color gridColor=new Color(150, 150,150);
+    Color gridColor=new Color(0, 0,0);
+    int[][][] map;
 
 
 
@@ -26,6 +27,7 @@ public class Main extends Applet implements Runnable, KeyListener {
         gfx=img.getGraphics();
         thread=new Thread(this);
         thread.start();
+        map=new int[128][90][3];
     }
 
     public void paint(Graphics g){
@@ -45,15 +47,55 @@ public class Main extends Applet implements Runnable, KeyListener {
         paint(g);
     }
 
+    int addtimer=100;
+    int movetimer=20;
     public void run() { for (;;){//CALLS UPDATES AND REFRESHES THE GAME
 
             //UPDATES
+            addtimer-=10;
+            movetimer-=10;
+            if (addtimer<0){
+                addtimer=0;
+                addBlock();
+                addBlock();
+                addBlock();
+                addBlock();
+                addBlock();
+                addBlock();
+                addBlock();
+            }
+            if (movetimer<0){
+                movetimer=10;
+                move();
+            }
 
 
             repaint();//UPDATES FRAME
-            try{ Thread.sleep(15); } //ADDS TIME BETWEEN FRAMES (FPS)
+            try{ Thread.sleep(50); } //ADDS TIME BETWEEN FRAMES (FPS)
             catch (InterruptedException e) { e.printStackTrace();System.out.println("GAME FAILED TO RUN"); }//TELLS USER IF GAME CRASHES AND WHY
     } }
+
+    public void addBlock(){
+        map[(int)(Math.random()*128)][0][0]=(int)(Math.random()*255);
+        map[(int)(Math.random()*128)][0][1]=(int)(Math.random()*255);
+        map[(int)(Math.random()*128)][0][2]=(int)(Math.random()*255);
+    }
+
+    public void move(){
+        for (int x=0; x<map.length; x++){
+            for (int y=map[0].length-1; y>-1; y--){
+                if (map[x][y][0]!=0) {
+                    int[] thisTile=map[x][y];
+                    if (y<map[0].length-1) {
+                        if (map[x][y + 1][0]==0) {
+                            map[x][y] = new int[]{0,0,0};
+                            map[x][y + 1] = thisTile;
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 
     //INPUT
@@ -67,14 +109,15 @@ public class Main extends Applet implements Runnable, KeyListener {
 
     //QUICK METHOD I MADE TO DISPLAY A COORDINATE GRID
     public void paintCoordGrid(Graphics gfx){
-        gfx.setColor(gridColor);
-        for (int x=100; x<WIDTH; x+=100){
-            gfx.drawString(""+x, x, 20);
-            gfx.drawRect(x, 20, 1, HEIGHT);
-        }
-        for (int y=100; y<HEIGHT; y+=100){
-            gfx.drawString(""+y, 20, y);
-            gfx.drawRect(20, y, WIDTH, 1);
+        int tilesize=10;
+
+        for (int x=0; x<map.length; x++){
+            for (int y=0; y<map[0].length; y++){
+                if (map[x][y][0]!=0) {
+                    gfx.setColor(new Color(map[x][y][0], map[x][y][1], map[x][y][2]));
+                    gfx.fillRect(tilesize * x, tilesize * y, tilesize, tilesize);
+                }
+            }
         }
     }
 }
