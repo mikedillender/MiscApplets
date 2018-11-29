5821965edfa67a120b9b9fee580753461325e781
CuseCose <dillemic000@gmail.com>
2018-11-29 16:09:58 -0600

    framework

--- src/Node.java
+++ src/Node.java
@@ -7,10 +7,119 @@ public class Node {
         switch (id){
             case 0:
                 pos=new int[]{5,1};
-                connections.add(new int[]{23,1});
+                connections.add(new int[]{1,1});
+                connections.add(new int[]{2,1});
                 break;
-            case 1:
-
+            case 2:
+                pos=new int[]{5,1};
+                connections.add(new int[]{1,1});
+                connections.add(new int[]{2,1});
+                break;
+            case 3:
+                pos=new int[]{5,1};
+                connections.add(new int[]{1,1});
+                connections.add(new int[]{2,1});
+                break;
+            case 4:
+                pos=new int[]{5,1};
+                connections.add(new int[]{1,1});
+                connections.add(new int[]{2,1});
+                break;
+            case 5:
+                pos=new int[]{5,1};
+                connections.add(new int[]{1,1});
+                connections.add(new int[]{2,1});
+                break;
+            case 6:
+                pos=new int[]{5,1};
+                connections.add(new int[]{1,1});
+                connections.add(new int[]{2,1});
+                break;
+            case 7:
+                pos=new int[]{5,1};
+                connections.add(new int[]{1,1});
+                connections.add(new int[]{2,1});
+                break;
+            case 8:
+                pos=new int[]{5,1};
+                connections.add(new int[]{1,1});
+                connections.add(new int[]{2,1});
+                break;
+            case 9:
+                pos=new int[]{5,1};
+                connections.add(new int[]{1,1});
+                connections.add(new int[]{2,1});
+                break;
+            case 10:
+                pos=new int[]{5,1};
+                connections.add(new int[]{1,1});
+                connections.add(new int[]{2,1});
+                break;
+            case 11:
+                pos=new int[]{5,1};
+                connections.add(new int[]{1,1});
+                connections.add(new int[]{2,1});
+                break;
+            case 12:
+                pos=new int[]{5,1};
+                connections.add(new int[]{1,1});
+                connections.add(new int[]{2,1});
+                break;
+            case 13:
+                pos=new int[]{5,1};
+                connections.add(new int[]{1,1});
+                connections.add(new int[]{2,1});
+                break;
+            case 14:
+                pos=new int[]{5,1};
+                connections.add(new int[]{1,1});
+                connections.add(new int[]{2,1});
+                break;
+            case 15:
+                pos=new int[]{5,1};
+                connections.add(new int[]{1,1});
+                connections.add(new int[]{2,1});
+                break;
+            case 16:
+                pos=new int[]{5,1};
+                connections.add(new int[]{1,1});
+                connections.add(new int[]{2,1});
+                break;
+            case 17:
+                pos=new int[]{5,1};
+                connections.add(new int[]{1,1});
+                connections.add(new int[]{2,1});
+                break;
+            case 18:
+                pos=new int[]{5,1};
+                connections.add(new int[]{1,1});
+                connections.add(new int[]{2,1});
+                break;
+            case 19:
+                pos=new int[]{5,1};
+                connections.add(new int[]{1,1});
+                connections.add(new int[]{2,1});
+                break;
+            case 20:
+                pos=new int[]{5,1};
+                connections.add(new int[]{1,1});
+                connections.add(new int[]{2,1});
+                break;
+            case 21:
+                pos=new int[]{5,1};
+                connections.add(new int[]{1,1});
+                connections.add(new int[]{2,1});
+                break;
+            case 22:
+                pos=new int[]{5,1};
+                connections.add(new int[]{1,1});
+                connections.add(new int[]{2,1});
+                break;
+            case 23:
+                pos=new int[]{5,1};
+                connections.add(new int[]{1,1});
+                connections.add(new int[]{2,1});
+                break;
         }
     }
 }
