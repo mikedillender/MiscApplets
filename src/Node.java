10e95d11b6d5109090b2598ec62582bd4ab73a65
CuseCose <dillemic000@gmail.com>
2018-11-29 16:07:09 -0600

    start

---
+++ src/Node.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Node {
+    int[] pos;
+    ArrayList<int[]> connections;
+    public Node(int id){
+        switch (id){
+            case 0:
+                pos=new int[]{5,1};
+                connections.add(new int[]{23,1});
+                break;
+            case 1:
+
+        }
+    }
+}
