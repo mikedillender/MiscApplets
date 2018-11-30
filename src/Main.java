a4448676af197e5f2a605f8ea407a943b497cc9b
CuseCose <dillemic000@gmail.com>
2018-11-30 12:55:48 -0600

    can now find optimal routes around the school and track distance

--- src/Main.java
+++ src/Main.java
@@ -13,9 +13,9 @@ public class Main extends Applet implements Runnable, KeyListener {
     private Thread thread;
     Graphics gfx;
     Image img;
-
-
     Node[] nodes;
+    int[][] paths;
+    int[] nodeUse;
 
     public void init(){//STARTS THE PROGRAM
         this.resize(sWIDTH, sHEIGHT);
@@ -25,12 +25,20 @@ public class Main extends Applet implements Runnable, KeyListener {
         thread.start();
         this.addKeyListener(this);
         nodes=new Node[25];
+        setPaths();
+        nodeUse=new int[25];
         for (int i=0; i<nodes.length; i++){
             nodes[i]=new Node(i);
         }
+        for (int i=0; i<nodes.length; i++){
+            nodes[i].addConnections(paths);
+        }
+
     }
 
     public void paint(Graphics g){
+        gfx.setColor(new Color(255,255,255));
+        gfx.fillRect(0,0,sWIDTH,sHEIGHT);
         draw(gfx);
         g.drawImage(img,0,0,this);
     }
@@ -50,8 +58,18 @@ public class Main extends Applet implements Runnable, KeyListener {
 
     public void draw(Graphics gfx){
         for (int i=0; i<nodes.length; i++){
-            gfx.drawString(i+"",100*nodes[i].getPos()[0],100*nodes[i].getPos()[1]);
-            gfx.drawRect(100*nodes[i].getPos()[0],100*nodes[i].getPos()[1], 10,10);
+            int size=10+(10*nodeUse[i]);
+            gfx.setColor(new Color(0,0,0));
+            gfx.fillRect(100*nodes[i].getPos()[0]-(size/2),100*nodes[i].getPos()[1]-(size/2), size, size);
+            gfx.drawString(i+"",100*nodes[i].getPos()[0]+size,100*nodes[i].getPos()[1]);
+        }
+        gfx.setColor(new Color(0,0,0));
+
+        for (int i=0; i<nodes.length; i++){
+            for (int c=0; c<nodes[i].connections.size(); c++){
+                gfx.drawLine(100*nodes[i].getPos()[0],100*nodes[i].getPos()[1],100*nodes[nodes[i].getConnections().get(c)[0]].getPos()[0],100*nodes[nodes[i].getConnections().get(c)[0]].getPos()[1]);
+
+            }
         }
     }
 
@@ -62,11 +80,127 @@ public class Main extends Applet implements Runnable, KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
+        getDayRoute();
+    }
 
+    public void getDayRoute(){
+        nodeUse=new int[25];
+        ArrayList<int[]> day=new ArrayList<>();
+        int[] s=new int[]{0,10,12,4,1,9,2};
+        int rt=0;
+        for (int p=1; p<s.length; p++){
+            int[] r=getFastestRoute(s[p-1],s[p]);
+            day.add(r);
+            for (int i=0; i<r.length; i++){
+                nodeUse[r[i]]++;
+                if (i>0){
+                    rt+=paths[getRouteBetween(r[i], r[i-1])][2];
+                }
+            }
+        }
+        System.out.println("total walking distance = "+rt);
+
+    }
+
+    private int getRouteBetween(int x, int y){
+        for (int i=0; i<paths.length; i++){
+            if ((paths[i][0]==x&&paths[i][1]==y)||(paths[i][0]==y&&paths[i][1]==x)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int[] getFastestRoute(int s, int dest){
+        int[] route =findRoute(s,dest,new int[0], true);
+        //if (route==null){return;}
+        System.out.println("");
+        System.out.print("Fastest route from "+s+" to "+dest+" is ");
+        for (int i=0; i<route.length; i++){
+            System.out.print(route[i]+", ");
+        }
+        return route;
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
 
+    }
+
+    public int[] findRoute(int cnode, int dest, int[] route, boolean isStart){
+        //System.out.println("cnode="+cnode);
+        for (int i=0; i<route.length; i++){if (cnode==route[i]){return null;}}
+        int[] r1=new int[route.length+1];
+        for (int i=0; i<route.length; i++){
+            r1[i]=route[i];
+        }
+        r1[route.length]=cnode;
+        if (cnode==dest){ return r1; }
+        ArrayList<int[]> psbleRoutes=new ArrayList<>();
+        for (int i=0; i<paths.length; i++){
+            if (paths[i][1]==cnode){
+                psbleRoutes.add(findRoute(paths[i][0],dest,r1,false));
+            }
+            if (paths[i][0]==cnode){
+                psbleRoutes.add(findRoute(paths[i][1],dest,r1,false));
+            }
+        }
+        int[] shortest=null;
+        for (int i=0; i<psbleRoutes.size(); i++){
+            if (psbleRoutes.get(i)!=null){
+                if (psbleRoutes.get(i)[psbleRoutes.get(i).length-1]==dest) {
+                    if (shortest==null){
+                        shortest=psbleRoutes.get(i);
+                    }else{
+                        if (psbleRoutes.get(i).length<shortest.length){
+                            shortest=psbleRoutes.get(i);
+                        }
+                    }
+                }
+            }
+        }
+        return shortest;
+
+    }
+
+    public void setPaths() {
+        paths=new int[][]{
+                {0,1,1},
+                {0,2,1},
+                {2,3,1},
+                {1,3,1},
+                {4,5,1},
+                {6,5,1},
+                {6,7,1},
+                {8,7,1},
+                {8,9,1},
+                {8,2,1},
+                {3,9,1},
+                {10,11,1},
+                {12,11,1},
+                {12,13,1},
+                {14,13,1},
+                {4,15,1},
+                {5,10,1},
+                {6,11,1},
+                {7,12,1},
+                {8,13,1},
+                {9,14,1},
+                {16,15,1},
+                {16,10,1},
+                {15,16,1},
+                {19,15,1},
+                {20,16,1},
+                {20,19,1},
+                {17,18,1},
+                {21,22,1},
+                {23,24,1},
+                {23,21,1},
+                {24,22,1},
+                {17,21,1},
+                {18,22,1},
+                {13,17,1},
+                {14,18,1},
+        };
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -3,139 +3,100 @@ import java.util.ArrayList;
 public class Node {
     int[] pos;
     ArrayList<int[]> connections;
+    int id;
     public Node(int id){
         connections=new ArrayList<>();
+        this.id=id;
         switch (id){
             case 0:
                 pos=new int[]{5,1};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
                 break;
             case 1:
                 pos=new int[]{6,1};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
                 break;
             case 2:
                 pos=new int[]{5,2};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
+
                 break;
             case 3:
                 pos=new int[]{6,2};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
+
                 break;
             case 4:
                 pos=new int[]{1,3};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
+
                 break;
             case 5:
                 pos=new int[]{2,3};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
+
                 break;
             case 6:
                 pos=new int[]{3,3};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
                 break;
             case 7:
                 pos=new int[]{4,3};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
                 break;
             case 8:
                 pos=new int[]{5,3};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
                 break;
             case 9:
                 pos=new int[]{6,3};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
                 break;
             case 10:
                 pos=new int[]{2,4};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
                 break;
             case 11:
                 pos=new int[]{3,4};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
                 break;
             case 12:
                 pos=new int[]{4,4};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
                 break;
             case 13:
                 pos=new int[]{5,4};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
                 break;
             case 14:
                 pos=new int[]{6,4};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
                 break;
             case 15:
                 pos=new int[]{1,5};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
                 break;
             case 16:
                 pos=new int[]{2,5};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
                 break;
             case 17:
                 pos=new int[]{5,6};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
                 break;
             case 18:
                 pos=new int[]{6,6};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
                 break;
             case 19:
                 pos=new int[]{1,6};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
                 break;
             case 20:
                 pos=new int[]{2,6};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
                 break;
             case 21:
                 pos=new int[]{5,7};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
                 break;
             case 22:
                 pos=new int[]{6,7};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
                 break;
             case 23:
                 pos=new int[]{5,8};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
                 break;
             case 24:
                 pos=new int[]{6,8};
-                connections.add(new int[]{1,1});
-                connections.add(new int[]{2,1});
                 break;
         }
     }
 
-    public void addConnections(){
+    public void addConnections(int[][] paths){
         connections=new ArrayList<>();
+        for (int i=0; i<paths.length; i++){
+            if (paths[i][0]==id||paths[i][1]==id){
+                connections.add(paths[i]);
+            }
+        }
 
     }
 
