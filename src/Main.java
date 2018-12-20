7c6fd037ef94c9a43f205ecc407244970ce64699
CuseCose <dillemic000@gmail.com>
2018-12-19 18:55:12 -0600

    initial

--- src/Main.java
+++ src/Main.java
@@ -12,12 +12,16 @@ public class Main extends Applet implements Runnable, KeyListener {
     private Thread thread;
     Graphics gfx;
     Image img;
+    long timeOfLastPull=0;
+    boolean isOn=true;
+
+    long msBetweenPulls=500;
+    float pullRange=30;
 
     //COLORS
     Color background=new Color(255, 255, 255);
-    Color gridColor=new Color(150, 150,150);
-
-
+    Color gridColor=new Color(0);
+    float[] rope=new float[100];
 
     public void init(){//STARTS THE PROGRAM
         this.resize(WIDTH, HEIGHT);
@@ -26,14 +30,14 @@ public class Main extends Applet implements Runnable, KeyListener {
         gfx=img.getGraphics();
         thread=new Thread(this);
         thread.start();
+        timeOfLastPull=System.currentTimeMillis();
     }
 
     public void paint(Graphics g){
         //BACKGROUND
         gfx.setColor(background);//background
         gfx.fillRect(0,0,WIDTH,HEIGHT);//background size
-        paintCoordGrid(gfx);
-
+        renderRope(gfx);
         //RENDER FOREGROUND
 
 
@@ -41,8 +45,42 @@ public class Main extends Applet implements Runnable, KeyListener {
         g.drawImage(img,0,0,this);
     }
 
+    public void renderRope(Graphics g){
+        g.setColor(gridColor);
+        int psize=WIDTH/rope.length;
+        int y=HEIGHT/2;
+        for (int i=0; i<rope.length; i++){
+            g.drawRect(psize*i,(int)(y+(psize*rope[i])), psize, psize);
+        }
+    }
+
     public void update(Graphics g){ //REDRAWS FRAME
+        if (isOn) {
+            long time = System.currentTimeMillis();
+            long dt = time - timeOfLastPull;
+            if (dt > msBetweenPulls) {
+                float str = -(pullRange/2f)+(float)(Math.random()*pullRange);
+                int loc = (int) (Math.random() * rope.length);
+                addPull(loc, str);
+                timeOfLastPull = time;
+            }
+        }
         paint(g);
+    }
+
+    private void addPull(int x, float str) {
+        System.out.println("pulling "+x+" with a strength of "+str);
+        for (int i = 0; i < rope.length; i++) {
+            float dist=(float)(Math.abs(x-i));
+            float mult=(dist>0)?1/dist:1.2f;
+            rope[i]=rope[i]+(str*mult);
+        }
+    }
+
+    private void normalize(){
+        for (int i=1; i<rope.length-1; i++){
+            rope[i]=(rope[i]+rope[i-1]+rope[i+1])/3f;
+        }
     }
 
     public void run() { for (;;){//CALLS UPDATES AND REFRESHES THE GAME
@@ -58,7 +96,12 @@ public class Main extends Applet implements Runnable, KeyListener {
 
     //INPUT
     public void keyPressed(KeyEvent e) {
-
+        if (e.getKeyCode()==KeyEvent.VK_P){
+            isOn=!isOn;
+        }
+        if (e.getKeyCode()==KeyEvent.VK_N){
+            normalize();
+        }
     }
     public void keyReleased(KeyEvent e) {
 
@@ -66,15 +109,5 @@ public class Main extends Applet implements Runnable, KeyListener {
     public void keyTyped(KeyEvent e) { }
 
     //QUICK METHOD I MADE TO DISPLAY A COORDINATE GRID
-    public void paintCoordGrid(Graphics gfx){
-        gfx.setColor(gridColor);
-        for (int x=100; x<WIDTH; x+=100){
-            gfx.drawString(""+x, x, 20);
-            gfx.drawRect(x, 20, 1, HEIGHT);
-        }
-        for (int y=100; y<HEIGHT; y+=100){
-            gfx.drawString(""+y, 20, y);
-            gfx.drawRect(20, y, WIDTH, 1);
-        }
-    }
+
 }
