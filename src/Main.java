e146c903de42dc79cb6b3f8cd84511b702eefeb5
CuseCose <dillemic000@gmail.com>
2019-01-15 18:02:48 -0600

    init

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 public class Main extends Applet implements Runnable, KeyListener {
 
@@ -18,27 +19,82 @@ public class Main extends Applet implements Runnable, KeyListener {
     Color gridColor=new Color(150, 150,150);
 
 
+    float[] world;
+    int points=100;
 
     public void init(){//STARTS THE PROGRAM
         this.resize(WIDTH, HEIGHT);
         this.addKeyListener(this);
+        world=new float[points];
+        for (int x=0; x<points; x++){
+            world[x]=100;
+        }
         img=createImage(WIDTH,HEIGHT);
         gfx=img.getGraphics();
         thread=new Thread(this);
         thread.start();
     }
 
+    public void change(){
+        for (int x=0; x<points; x++){
+            float change=(float)(Math.random()*4)-2;
+            world[x]+=change;
+        }
+    }
+    public void smooth(){
+        for (int x=0; x<points; x++){
+            int samples=0;
+            float sum=0;
+            for (int x1=x-1; x1<=x+1; x1++){
+                if (exists(x1)){
+                    samples++;
+                    sum+=world[x1];
+                }if (x1>=points){
+                    samples++;
+                    sum+=world[0];
+                }else if (x1<0){
+                    samples++;
+                    sum+=world[world.length+x1];
+                }
+
+            }
+            world[x]=sum/samples;
+        }
+    }
+    private boolean exists(int i){
+        if (i>0&&i<points){return true;}
+        return false;
+    }
+
     public void paint(Graphics g){
         //BACKGROUND
         gfx.setColor(background);//background
         gfx.fillRect(0,0,WIDTH,HEIGHT);//background size
-        paintCoordGrid(gfx);
-
-        //RENDER FOREGROUND
-
+        renderCircle(gfx);
+        change();
+        smooth();
 
         //FINAL
         g.drawImage(img,0,0,this);
+    }
+
+    public void renderCircle(Graphics g){
+        float rad=200;
+        int cx=WIDTH/2;
+        int cy=HEIGHT/2;
+        g.setColor(Color.BLACK);
+        ArrayList<int[]> p=new ArrayList<>();
+        for (int i=0; i<points; i++){
+                float o=((float)i/points)*6.28f;
+                float x=cx+(int)(world[i]*Math.cos(o));
+                float y=cy+(int)(world[i]*Math.sin(o));
+                p.add(new int[]{(int)x,(int)y});
+        }
+        for (int i=0; i<p.size()-1; i++){
+            g.drawLine(p.get(i)[0],p.get(i)[1],p.get(i+1)[0],p.get(i+1)[1]);
+        }
+        g.drawLine(p.get(0)[0],p.get(0)[1],p.get(p.size()-1)[0],p.get(p.size()-1)[1]);
+
     }
 
     public void update(Graphics g){ //REDRAWS FRAME
@@ -65,16 +121,4 @@ public class Main extends Applet implements Runnable, KeyListener {
     }
     public void keyTyped(KeyEvent e) { }
 
-    //QUICK METHOD I MADE TO DISPLAY A COORDINATE GRID
-    public void paintCoordGrid(Graphics gfx){
-        gfx.setColor(gridColor);
-        for (int x=100; x<WIDTH; x+=100){
-            gfx.drawString(""+x, x, 20);
-            gfx.drawRect(x, 20, 1, HEIGHT);
-        }
-        for (int y=100; y<HEIGHT; y+=100){
-            gfx.drawString(""+y, 20, y);
-            gfx.drawRect(20, y, WIDTH, 1);
-        }
-    }
 }
