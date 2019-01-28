484b983d771f2176c83e1a18c03cc93d1b75dff8
CuseCose <dillemic000@gmail.com>
2019-01-28 13:11:12 -0600

    init

--- src/Main.java
+++ src/Main.java
@@ -32,7 +32,6 @@ public class Main extends Applet implements Runnable, KeyListener {
         //BACKGROUND
         gfx.setColor(background);//background
         gfx.fillRect(0,0,WIDTH,HEIGHT);//background size
-        paintCoordGrid(gfx);
 
         //RENDER FOREGROUND
 
@@ -55,10 +54,49 @@ public class Main extends Applet implements Runnable, KeyListener {
             catch (InterruptedException e) { e.printStackTrace();System.out.println("GAME FAILED TO RUN"); }//TELLS USER IF GAME CRASHES AND WHY
     } }
 
+    private Color getRandomSkin(){
+        float r=0,g=0,b=0;
+
+        while (!isValidSkin(r,g,b)) {
+            float maxDiff = .2f;
+            r = random(220, 20);
+            g = random(r - 40, 20);
+            b = random(g - 40, 20);
+        }
+        return new Color(r/255f,g/255f,b/255f,1);
+
+    }
+
+    private Color varL(Color c){
+        float r=c.getRed(), g=c.getGreen(), b=c.getBlue();
+        float p=1-(float)(Math.random()*Math.random()*.75);
+        System.out.println("p = "+p);
+        System.out.printf((r*p)+", "+(g*p)+","+(b*p));
+        return new Color(r*p/255f,g*p/255f,b*p/255f);
+    }
+
+
+
+    private boolean isValidSkin(float r, float g, float b){
+        float avgVal=(r+g+b)/(3);
+        if (avgVal>175&&r>g&&g>b){
+            return true;
+        }
+        return false;
+    }
+
+    private float random(float around, float maxdiff){
+        float d=(float)(Math.random()*2*maxdiff)-maxdiff;
+        return around+d;
+    }
 
     //INPUT
     public void keyPressed(KeyEvent e) {
-
+        if (e.getKeyCode()==KeyEvent.VK_C) {
+            background = getRandomSkin();
+        }else if (e.getKeyCode()==KeyEvent.VK_D){
+            background=varL(background);
+        }
     }
     public void keyReleased(KeyEvent e) {
 
@@ -66,15 +104,5 @@ public class Main extends Applet implements Runnable, KeyListener {
     public void keyTyped(KeyEvent e) { }
 
     //QUICK METHOD I MADE TO DISPLAY A COORDINATE GRID
-    public void paintCoordGrid(Graphics gfx){
-        gfx.setColor(gridColor);
-        for (int x=100; x<WIDTH; x+=100){
-            gfx.drawString(""+x, x, 20);
-            gfx.drawRect(x, 20, 1, HEIGHT);
-        }
-        for (int y=100; y<HEIGHT; y+=100){
-            gfx.drawString(""+y, 20, y);
-            gfx.drawRect(20, y, WIDTH, 1);
-        }
-    }
+
 }
