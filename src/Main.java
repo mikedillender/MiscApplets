755dfe372b97eb3ae9ac29714ee04e7f57ac38c6
CuseCose <dillemic000@gmail.com>
2019-06-24 21:50:19 -0400

    wow this wont work
    y dont I own a supercomputer ;(

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,11 @@ public class Main extends Applet implements Runnable, KeyListener, MouseListener
 
     //BASIC VARIABLES
     private final int WIDTH=1000, HEIGHT=1000;
-    int width=20;
+    int width=16;
     int size=(int)(Math.floor(WIDTH/width));
 
-    int sizeindex=1;
-    int[] sizes=new int[]{2,4,8,12,16};
+    int sizeindex=4;
+    int[] sizes=new int[]{2,4,8,12,16,20,24,28,32};
 
     //GRAPHICS OBJECTS
     private Thread thread;
@@ -26,8 +26,8 @@ public class Main extends Applet implements Runnable, KeyListener, MouseListener
     Color gridColor=new Color(150, 23, 13);
     Color red=new Color(83, 2, 1);
     Color lightred=new Color(209, 157, 160);
-    Color green=new Color(1, 55, 2);
-    Color lightgreen=new Color(168, 209, 165);
+    Color green=new Color(1, 34, 1);
+    Color lightgreen=new Color(209, 242, 205);
     Color grey=new Color(122, 122, 122);
     Color black=new Color(0,0,0);
 
@@ -46,16 +46,16 @@ public class Main extends Applet implements Runnable, KeyListener, MouseListener
     }
 
 
-    public void create(boolean random){
-        while (!make(random)){}
+    public void create(boolean random,boolean symm){
+        while (!make(random,symm)){}
     }
 
     public void init(){//STARTS THE PROGRAM
         this.resize(WIDTH, HEIGHT);
         this.addKeyListener(this);
         this.addMouseListener(this);
-        //make(false);
-        create(true);
+        //make(false,false);
+        //create(true);
         img=createImage(WIDTH,HEIGHT);
         gfx=img.getGraphics();
         thread=new Thread(this);
@@ -70,6 +70,7 @@ public class Main extends Applet implements Runnable, KeyListener, MouseListener
         return row;
     }
 
+
     public boolean[] getRowID(int id){
         int r=id;
         boolean[] row=new boolean[m.length];
@@ -83,6 +84,37 @@ public class Main extends Applet implements Runnable, KeyListener, MouseListener
         }
         return row;
     }
+    public boolean[] getSymRowID(int id, int rn){
+        /*int r=id;
+        boolean[] row=new boolean[m.length];
+        for (int i=row.length-1; i>=0; i--){
+            int pow=(int)(Math.pow(2,i-rn));
+            //System.out.println("int id "+id+" y="+(width-1-i)+" i="+i+" pow = "+pow+" r="+r);
+            row[width-1-i]=r>=pow;
+            if (row[width-1-i]){
+                r-=pow;
+            }
+        }
+        for (int x=0; x<rn; x++){
+            row[x]=m[x][rn];
+        }*/
+        int r=id;
+        boolean[] row=new boolean[m.length];
+        for (int i=row.length-1; i>=0; i--){
+            int pow=(int)(Math.pow(2,i));
+            //System.out.println("int id "+id+" y="+(width-1-i)+" i="+i+" pow = "+pow+" r="+r);
+            row[width-1-i]=r>=pow;
+            if (row[width-1-i]){
+                r-=pow;
+            }
+        }
+        for (int x=0; x<rn; x++){
+            row[x]=m[x][rn];
+        }
+        return row;
+    }
+
+
 
     public boolean checkRow(int i){
         boolean works = true;
@@ -94,39 +126,45 @@ public class Main extends Applet implements Runnable, KeyListener, MouseListener
         return works;
     }
 
-    public boolean make(boolean rand){
+    public boolean make(boolean rand, boolean symm){
 
         int allIds=(int)(Math.pow(2,width));
 
         ArrayList<Integer>[] prohib=new ArrayList[width];
         int id=0;
-
+        prohib[0]=new ArrayList<>();
         int[] startids=new int[width];
+        int incSize=(int)(Math.ceil(allIds)/(width*width));
         for (int i=1; i<m.length; i++){
             if (prohib[i]==null){prohib[i]=new ArrayList<>();}
+            if (symm){
+            //    allIds=(int)(Math.pow(2,width-i));
+            }
             startids[i]=id;
             System.out.println("row "+i);
             boolean done=false;
             long s=System.nanoTime();
             int lastId=id;
             int iter=0;
+            //if (symm){id=0;}
             while ((id<allIds||rand)&&!done) {
                 if (rand){
-                    iter++;
                     m[i]=getRandomRow();
-                }else {
-                    id++;
+                }else{
                     for (Integer pid:prohib[i]){
                         if (pid-id==0){
                             id++;
                         }
                     }
-                    m[i] = getRowID(id);
+                    m[i] = (!symm)?getRowID(id):getSymRowID(id,i);
                 }
-                if ((id%1000000==0&&!rand)||(rand&&iter%1000000==0)){
+
+                if ((id%incSize==0&&!rand&&id>1000000)){
                     System.out.println(id+"/"+allIds+" ( "+Math.round(10000f*(id/(float)allIds))/100.0+"% )");
+                }else if ((rand&&iter%incSize==0&&iter>1000000)){
+                    System.out.println(iter+" iterations");
                     double dt=((System.nanoTime()-s)/1000000000.0);
-                    if (dt>5&&rand){
+                    if (dt>4*i){
                         return false;
                     }
                 }
@@ -141,12 +179,18 @@ public class Main extends Applet implements Runnable, KeyListener, MouseListener
                     startids[i]=id;
                     done=true;
                 }
+                id++;
+                iter++;
+
             }
             if (!rand) {
                 if (id == allIds) {
-                    prohib[i - 1].add(lastId);
-                    id = startids[i - 1];
-                    i = i - 2;
+                    prohib[i]=new ArrayList<>();
+                    if (i!=0) {
+                        prohib[i - 1].add(lastId);
+                        id = startids[i - 1];
+                        i = i - 2;
+                    }
                 }
             }
         }
@@ -159,7 +203,9 @@ public class Main extends Applet implements Runnable, KeyListener, MouseListener
         gfx.setColor(background);//background
         gfx.fillRect(0,0,WIDTH,HEIGHT);//background size
 
+
         gfx.setColor(Color.BLACK);
+
         //RENDER FOREGROUND
         for (int x=0; x<m.length; x++){
             if (checkRow(x)){
@@ -201,7 +247,7 @@ public class Main extends Applet implements Runnable, KeyListener, MouseListener
     } }
 
     private void createNew(boolean rand){
-        make(rand);
+        make(rand,false);
     }
 
     private void randomize(float percent){
@@ -209,7 +255,7 @@ public class Main extends Applet implements Runnable, KeyListener, MouseListener
         for (int x=0; x<width; x++){
             System.out.println("");
             for (int y=0; y<width; y++){
-                if (percent<Math.random()){
+                if (percent>Math.random()){
                     m[x][y]=Math.random()<.5;
                     System.out.print("("+x+","+y+") ");
                 }
@@ -221,10 +267,32 @@ public class Main extends Applet implements Runnable, KeyListener, MouseListener
     public void keyPressed(KeyEvent e) {
         System.out.println("pressed "+e.getKeyCode());
         if (e.getKeyCode()==KeyEvent.VK_M) {
-            make(false);
+            create(false,false);
         }
         if (e.getKeyCode()==KeyEvent.VK_N) {
-            make(true);
+           create(true,false);
+        }
+        if (e.getKeyCode()==KeyEvent.VK_S) {
+            make(false,true);
+        }
+        if (e.getKeyCode()==KeyEvent.VK_RIGHT) {
+            if (sizeindex+1<sizes.length) {
+                sizeindex++;
+                width=sizes[sizeindex];
+                System.out.println("size = "+width);
+                size=(int)(Math.floor(WIDTH/width));
+                m=new boolean[width][width];
+            }
+        }
+        if (e.getKeyCode()==KeyEvent.VK_LEFT) {
+            if (sizeindex-1>=0) {
+                sizeindex--;
+                width=sizes[sizeindex];
+                System.out.println("size = "+width);
+                size=(int)(Math.floor(WIDTH/width));
+                m=new boolean[width][width];
+            }
+
         }
         if (e.getKeyCode()<=57&&e.getKeyCode()>=48){
             float p=(e.getKeyCode()-48f)/10f;
