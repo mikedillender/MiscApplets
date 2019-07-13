44a3141aa4d70e0f1262fafffe4b76883eed5f27
CuseCose <dillemic000@gmail.com>
2019-07-12 19:04:50 -0500

    rope gang

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
+import com.sun.javafx.geom.Vec2d;
+
 import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
-public class Main extends Applet implements Runnable, KeyListener {
+public class Main extends Applet implements Runnable, KeyListener, MouseListener {
 
     //BASIC VARIABLES
-    private final int WIDTH=1280, HEIGHT=900;
+    private final int WIDTH=1280, HEIGHT=700;
 
     //GRAPHICS OBJECTS
     private Thread thread;
@@ -17,11 +22,18 @@ public class Main extends Applet implements Runnable, KeyListener {
     Color background=new Color(255, 255, 255);
     Color gridColor=new Color(150, 150,150);
 
+    Vec2d s=new Vec2d(75,HEIGHT/3f);
+    Vec2d e=new Vec2d(1000,700);
+    ArrayList<Vec2d> rope=new ArrayList<>();
+    float L=(float)(Math.sqrt((WIDTH*WIDTH)+((HEIGHT*HEIGHT)/4f)));
+
+    float sbdy=-50;
 
 
     public void init(){//STARTS THE PROGRAM
         this.resize(WIDTH, HEIGHT);
         this.addKeyListener(this);
+        this.addMouseListener(this);
         img=createImage(WIDTH,HEIGHT);
         gfx=img.getGraphics();
         thread=new Thread(this);
@@ -32,8 +44,8 @@ public class Main extends Applet implements Runnable, KeyListener {
         //BACKGROUND
         gfx.setColor(background);//background
         gfx.fillRect(0,0,WIDTH,HEIGHT);//background size
-        paintCoordGrid(gfx);
-
+        //paintCoordGrid(gfx);
+        paintRope(gfx);
         //RENDER FOREGROUND
 
 
@@ -55,10 +67,124 @@ public class Main extends Applet implements Runnable, KeyListener {
             catch (InterruptedException e) { e.printStackTrace();System.out.println("GAME FAILED TO RUN"); }//TELLS USER IF GAME CRASHES AND WHY
     } }
 
+    public void paintRope(Graphics g){
+        g.setColor(Color.BLACK);
+        if (rope.size()==0){
+            g.fillRect((int)s.x,HEIGHT-(int)s.y,5,5);
+            g.fillRect((int)e.x,HEIGHT-(int)e.y,5,5);
+        }else {
+            for (Vec2d v: rope){
+                g.fillRect((int)v.x,HEIGHT-(int)v.y,5,5);
+            }
+            for (int i=1; i<rope.size();i++){
+                g.drawLine((int)rope.get(i-1).x,HEIGHT-(int)rope.get(i-1).y,(int)rope.get(i).x,HEIGHT-(int)rope.get(i).y);
+            }
+        }
+
+    }
+
+
+    public void setRope(float bdy){
+        rope=new ArrayList<>();
+        int numPoints=30;
+        float dx= (float) (e.x-s.x)/2f;
+        Vec2d b=new Vec2d(s.x+dx, s.y+bdy);
+        float dy= (float) (s.y-b.y);
+        rope.add(s);
+        if (bdy<0) {
+            for (int i = 1; i < numPoints; i++) {
+                float x = (((1 - (i / (float) numPoints)) * dx));
+                float y2 = (1 - ((x * x) / (dx * dx))) * (dy * dy);
+                rope.add(new Vec2d(s.x + dx - x, s.y - (float) (Math.sqrt(Math.abs(y2)))));
+            }
+            dy = (float) (e.y - b.y);
+            for (int i = 0; i < numPoints; i++) {
+                float x = ((((i / (float) numPoints)) * dx));
+                float y2 = (1 - ((x * x) / (dx * dx))) * (dy * dy);
+                rope.add(new Vec2d(s.x + dx + x, e.y - (float) (Math.sqrt(Math.abs(y2)))));
+            }
+        }else {
+            dx*=2;
+            dy= (float) (e.y-s.y);
+            numPoints*=2;
+            for (int i = 0; i < numPoints; i++) {
+                float x = ((((i / (float) numPoints)) * dx));
+                float y2 = (1 - ((x * x) / (dx * dx))) * (dy * dy);
+                rope.add(new Vec2d(s.x + x, e.y - (float) (Math.sqrt(Math.abs(y2)))));
+            }
+        }
+        rope.add(e);
+    }
+
+    public void setRopeStraight(){
+        rope=new ArrayList<>();
+        int numPoints=30;
+        float dx= (float) (e.x-s.x);
+        float dy= (float) (e.y-s.y);
+        float m=(dy/dx);
+        rope.add(s);
+        for (int i=0; i<numPoints;i++){
+            float x=((float)i/numPoints)*dx;
+            float y=m*x;
+            rope.add(new Vec2d(s.x+x,s.y+y));
+        }
+        rope.add(e);
+
+
+    }
+
+
+    public void sag(){
+        float amt=5;
+        for (int i=1; i<rope.size()-1; i++){
+            rope.get(i).y-=amt;
+        }
+    }
+
+    public float getRopeL(){
+        float l=0;
+        for (int i=1; i<rope.size(); i++){
+            float dx= (float) (rope.get(i).x-rope.get(i-1).x);
+            float dy= (float) (rope.get(i).y-rope.get(i-1).y);
+            float d=(float)(Math.sqrt(dx*dx+dy*dy));
+            l=l+d;
+        }
+        //System.out.println(l);
+        return l;
+    }
+
+    public void average(){
+        if (rope.size()==0){return;}
+        ArrayList<Vec2d> newr=new ArrayList<>();
+        newr.add(rope.get(0));
+        for (int i=1; i<rope.size()-1; i++){
+            float y= (float) (rope.get(i).y+rope.get(i-1).y+rope.get(i+1).y)/3f;
+            newr.add(new Vec2d(rope.get(i).x,y));
+        }
+        newr.add(rope.get(rope.size()-1));
+        rope=newr;
+    }
+
+
 
     //INPUT
     public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode()==KeyEvent.VK_S) {
+            setRopeStraight();
+        }else if (e.getKeyCode()==KeyEvent.VK_SPACE){
+            average();
+        }else if(e.getKeyCode()==KeyEvent.VK_L){
+            System.out.println(getRopeL());
+        }else if(e.getKeyCode()==KeyEvent.VK_G){
+            sag();
+        }else if(e.getKeyCode()==KeyEvent.VK_UP){
+            sbdy-=10;
+        }else if(e.getKeyCode()==KeyEvent.VK_DOWN){
+            sbdy-=10;
+        }
 
+        System.out.println("BDY = "+sbdy+", length = "+getRopeL()+" / "+L);
+        System.out.println("");
     }
     public void keyReleased(KeyEvent e) {
 
@@ -76,5 +202,60 @@ public class Main extends Applet implements Runnable, KeyListener {
             gfx.drawString(""+y, 20, y);
             gfx.drawRect(20, y, WIDTH, 1);
         }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        this.e=new Vec2d(e.getX(),HEIGHT-e.getY());
+        setRopeLength();
+        //setRope(sbdy);
+        //while (Math.abs(getRopeL()-L)>50){
+        //    setRope((float) (-HEIGHT+(2*Math.random()*HEIGHT)));
+        //}
+        System.out.println("BDY = "+sbdy+", length = "+getRopeL()+" / "+L);
+        System.out.println("");
+    }
+
+    public void setRopeLength(){
+        setRope(sbdy);
+        float lastL=getRopeL();
+        int attempts=0;
+        while (Math.abs(lastL-L)>50&&attempts<100){
+            attempts++;
+            if (sbdy>0&&L>lastL){
+                average();
+            }else if (lastL<L){
+                sbdy-=10;
+                setRope(sbdy);
+            }else {
+                sbdy+=10;
+                setRope(sbdy);
+            }
+            average();
+            average();
+
+            System.out.println("BDY = "+sbdy+", length = "+getRopeL()+" / "+L);
+            lastL=getRopeL();
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
