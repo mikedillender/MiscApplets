2f04fc71ba32fa466b22c3ac65fe061d728f01a3
CuseCose <dillemic000@gmail.com>
2019-07-18 03:15:38 -0500

    its basic, but it works

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import com.sun.javafx.geom.Vec2d;
+
 import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 public class Main extends Applet implements Runnable, KeyListener {
 
@@ -17,6 +20,9 @@ public class Main extends Applet implements Runnable, KeyListener {
     Color background=new Color(255, 255, 255);
     Color gridColor=new Color(150, 150,150);
 
+    ArrayList<float[]> smoke;
+    float direction=0;
+    Vec2d valve;
 
 
     public void init(){//STARTS THE PROGRAM
@@ -24,18 +30,65 @@ public class Main extends Applet implements Runnable, KeyListener {
         this.addKeyListener(this);
         img=createImage(WIDTH,HEIGHT);
         gfx=img.getGraphics();
+        reset(0);
         thread=new Thread(this);
         thread.start();
+    }
+
+    public void reset(float direction){
+        this.direction=direction;
+        smoke=new ArrayList<>();
+        valve=new Vec2d(WIDTH/2f,HEIGHT/2f);
+    }
+
+    public void addRandom(){
+        float o= (float) (direction+(((Math.random()<.5f)?1:-1)*(Math.random()*Math.random()*3.14159/6)));
+        //System.out.println("d = "+direction+", o = "+o);
+        float v=4;
+        float size=(float) (50+(((Math.random()<.5f)?1:-1)*(Math.random()*Math.random()*30)));
+        float[] s=new float[]{(float) valve.x, (float) valve.y, (float)(Math.cos(o)*v), (float)(Math.sin(o)*v), 2,size};
+        smoke.add(s);
+    }
+
+    int timer=10;
+
+    public void move() {
+        for (float[] s : smoke) {
+            s[4]-=.015f;
+            s[0]+=s[2];
+            s[1]+=s[3];
+            s[3]-=.1f;
+            s[5]+=.5f;
+        }
+        for (int i=0; i<smoke.size(); i++){
+            if (smoke.get(i)[4]<0){
+                smoke.remove(i);
+                i--;
+            }
+        }
     }
 
     public void paint(Graphics g){
         //BACKGROUND
         gfx.setColor(background);//background
         gfx.fillRect(0,0,WIDTH,HEIGHT);//background size
-        paintCoordGrid(gfx);
+        //paintCoordGrid(gfx);
 
+        /*timer--;
+        if (timer<0){
+            timer=10;*/
+            addRandom();
+        //}
+        move();
         //RENDER FOREGROUND
-
+        for (float[] s: smoke){
+            float c=.5f+.5f*((1-(s[4]/2f)));
+            //System.out.println(c);
+            gfx.setColor(new Color(c,c,c));
+            gfx.fillOval((int)s[0],(int)s[1],(int)s[5],(int)s[5]);
+        }
+        gfx.setColor(Color.BLACK);
+        gfx.drawString("size : "+smoke.size(), 100,100);
 
         //FINAL
         g.drawImage(img,0,0,this);
@@ -58,7 +111,19 @@ public class Main extends Applet implements Runnable, KeyListener {
 
     //INPUT
     public void keyPressed(KeyEvent e) {
-
+        if (e.getKeyCode()==KeyEvent.VK_UP){
+            direction=3.14f/2f;
+            reset(direction);
+        }if (e.getKeyCode()==KeyEvent.VK_RIGHT){
+            direction=0;
+            reset(direction);
+        }if (e.getKeyCode()==KeyEvent.VK_DOWN){
+            direction=(3.14159f/2)*3;
+            reset(direction);
+        }if (e.getKeyCode()==KeyEvent.VK_LEFT){
+            direction=3.14159f;
+            reset(direction);
+        }
     }
     public void keyReleased(KeyEvent e) {
 
