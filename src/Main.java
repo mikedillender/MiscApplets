335750591cba7a31981910911d1dfbc60aeaee43
CuseCose <dillemic000@gmail.com>
2019-11-22 10:51:06 -0600

    I mean its kinda cool, but not exactly what I wanted

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import com.sun.javafx.geom.Vec2f;
+
 import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 public class Main extends Applet implements Runnable, KeyListener {
 
@@ -16,14 +19,17 @@ public class Main extends Applet implements Runnable, KeyListener {
     //COLORS
     Color background=new Color(255, 255, 255);
     Color gridColor=new Color(150, 150,150);
-
-
-
+    float timer=1;
+    ArrayList<Node> n;
+    ArrayList<ArrayList<Node>> ss;
     public void init(){//STARTS THE PROGRAM
         this.resize(WIDTH, HEIGHT);
         this.addKeyListener(this);
         img=createImage(WIDTH,HEIGHT);
+        n=new ArrayList<>();
+        ss=new ArrayList<>();
         gfx=img.getGraphics();
+        createAll();
         thread=new Thread(this);
         thread.start();
     }
@@ -32,8 +38,28 @@ public class Main extends Applet implements Runnable, KeyListener {
         //BACKGROUND
         gfx.setColor(background);//background
         gfx.fillRect(0,0,WIDTH,HEIGHT);//background size
-        paintCoordGrid(gfx);
+        //paintCoordGrid(gfx);
+        gfx.setColor(Color.BLACK);
+        /*for (Node no:n){
+            for (Node cn:no.connected){
+                gfx.drawLine((int)(no.pos.x),(int)(no.pos.y),(int)(cn.pos.x),(int)(cn.pos.y));
+            }
+        }*/
+        int l1=0;
+        for (ArrayList<Node> s:ss){
+            l1++;
+            int i=0;
+            int[] x=new int[s.size()];
+            int[] y=new int[s.size()];
 
+            for (Node sn: s){
+                x[i]=(int)(sn.pos.x);
+                y[i]=(int)(sn.pos.y);
+                i++;
+            }
+            gfx.setColor(new Color((l1*255/ss.size())/2,(l1*255/ss.size())/2,(l1*255/ss.size())));
+            gfx.fillPolygon(x,y,s.size());
+        }
         //RENDER FOREGROUND
 
 
@@ -45,10 +71,73 @@ public class Main extends Applet implements Runnable, KeyListener {
         paint(g);
     }
 
+    public void createAll(){
+        int pw=15;
+        int py=10;
+        for (int y=0; y<py; y++){
+            boolean out=(y==0||y+1==py);
+            for (int x=0;x <pw; x++){
+                boolean out1= (out) ||( x == 0 )||( x + 1 == pw);
+                System.out.println("x = "+x+", y = "+y+" | out = "+out);
+                Node nw=new Node(new Vec2f(x*((float)(WIDTH+100f)/(pw-1))-50,y*((float)(HEIGHT+100f)/(py-1))-50),new Vec2f((float)(Math.random()*14-7),(float)(Math.random()*14-7)),out1);
+                nw.connectClosest(2,n);
+                n.add(nw);
+            }
+        }
+        for (Node n1:n){
+            ss.add(findShape(n1));
+
+        }
+    }
+
+    public ArrayList<Node> findShape(Node n){
+        //ArrayList<A>
+        ArrayList<Node> s=new ArrayList<>();
+        if (n.connected.size()>1) {
+            s.add(n);
+            for (Node n1:n.connected){
+                s.add(n1);
+            }
+        }
+        return s;
+    }
+
+    public void addShape(){
+        int size=3+(int)(Math.random()*3);
+        ArrayList<Node> s=new ArrayList<>();
+        int rx=(int)(0);
+        int ry=(int)(Math.random()*HEIGHT);
+        for (int i=0; i<size; i++){
+            /*Node sn=new Node(new Vec2f(rx-50+(int)(Math.random()*100),ry-50+(int)(Math.random()*100)),new Vec2f((float)(Math.random()*13+7),(float)(Math.random()*18-9)));
+            for (int z=0; z<s.size(); z++){
+                sn.connectTo(s.get(z));
+            }
+            s.add(sn);*/
+        }
+        for (Node sn:s){
+            sn.connectClosest(5,n);
+            n.add(sn);
+        }
+        ss.add(s);
+    }
+
     public void run() { for (;;){//CALLS UPDATES AND REFRESHES THE GAME
 
             //UPDATES
-
+            timer-=.015f;
+            if (timer<0){
+                timer=.5f;
+                //if (n.size()>4) {
+                //    ss.add(findShape(n.get((int) (n.size() * Math.random()))));
+                //}
+                //addShape();
+                //Node nw=new Node(new Vec2f(0,(float)(Math.random()*HEIGHT)),new Vec2f((float)(Math.random()*13+7),(float)(Math.random()*18-9)));
+                //nw.connectClosest(4,n);
+                //n.add(nw);
+            }
+            for (Node no:n){
+                no.update(.015f);
+            }
 
             repaint();//UPDATES FRAME
             try{ Thread.sleep(15); } //ADDS TIME BETWEEN FRAMES (FPS)
@@ -58,7 +147,7 @@ public class Main extends Applet implements Runnable, KeyListener {
 
     //INPUT
     public void keyPressed(KeyEvent e) {
-
+        //addShape();
     }
     public void keyReleased(KeyEvent e) {
 
---
+++ src/Node.java
@@ -0,0 +1,64 @@
+import com.sun.javafx.geom.Vec2f;
+import com.sun.javafx.geom.Vec3f;
+
+import java.util.ArrayList;
+
+public class Node {
+
+    ArrayList<Node> connected=new ArrayList<>();
+    Vec2f pos;
+    Vec2f vel;
+    boolean outside;
+
+    public Node(Vec2f p, Vec2f v, boolean outside){
+        this.vel=v;
+        this.pos=p;
+        this.outside=outside;
+    }
+    public void update(float delta){
+        if (outside){return;}
+        pos.x+=vel.x*delta;
+        pos.y+=vel.y*delta;
+        for (Node n:connected){
+            //float di=n.distTo(this);
+            vel.x+=(delta*(n.pos.x-this.pos.x)/20);
+            vel.y+=(delta*(n.pos.y-this.pos.y)/20);
+        }
+    }
+    public void connectClosest(int num, ArrayList<Node> nodes){
+        //connected=new ArrayList<>();
+        num=num-connected.size();
+        for (int i=0; i<num; i++){
+            Node b=null;
+            float bd=10000;
+            for(Node n: nodes){
+                if (n!=this && !connected.contains(n)){
+                    float d=n.distTo(this);
+                    if (d<bd){
+                        bd=d;
+                        b=n;
+                    }
+                }
+            }
+            if (b!=null){
+                connectTo(b);
+            }
+        }
+
+    }
+
+    public float distTo(Node n){
+        return pos.distance(n.pos);
+    }
+
+    public void connectTo(Node n){
+        if(n==this){return;}
+        if (!connected.contains(n)) {
+            connected.add(n);
+        }
+        if (!n.connected.contains(this)) {
+            n.connected.add(this);
+        }
+    }
+
+}
