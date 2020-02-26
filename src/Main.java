65a67656e89faa4aec09d86d20e740e21e8d4747
CuseCose <dillemic000@gmail.com>
2020-02-26 12:36:11 -0600

    Working generation and movement

--- src/Main.java
+++ src/Main.java
@@ -17,13 +17,22 @@ public class Main extends Applet implements Runnable, KeyListener {
     Color background=new Color(255, 255, 255);
     Color gridColor=new Color(150, 150,150);
 
+    float[][] vecs;
+    int px=0,py=0;
 
+    public void createVecs(int num){
+        vecs=new float[num][4];
+        for (int i=0; i<num*4; i++){
+            vecs[i/4][i%4]=(float)(Math.random()*10-5);
+        }
+    }
 
     public void init(){//STARTS THE PROGRAM
         this.resize(WIDTH, HEIGHT);
         this.addKeyListener(this);
         img=createImage(WIDTH,HEIGHT);
         gfx=img.getGraphics();
+        createVecs(1);
         thread=new Thread(this);
         thread.start();
     }
@@ -35,7 +44,14 @@ public class Main extends Applet implements Runnable, KeyListener {
         paintCoordGrid(gfx);
 
         //RENDER FOREGROUND
-
+        int ts=30;
+        for (int x=0; x<WIDTH/ts; x++){
+            for (int y=0; y<HEIGHT/ts; y++) {
+                int v=getTile(x+px,y+py);
+                gfx.setColor(new Color((v/(255*255)),(v/255)%255,v%255));
+                gfx.fillRect(ts*x,ts*y,ts,ts);
+            }
+        }
 
         //FINAL
         g.drawImage(img,0,0,this);
@@ -58,12 +74,34 @@ public class Main extends Applet implements Runnable, KeyListener {
 
     //INPUT
     public void keyPressed(KeyEvent e) {
-
+        if (e.getKeyCode()==KeyEvent.VK_UP){
+            py++;
+        }if (e.getKeyCode()==KeyEvent.VK_DOWN){
+            py--;
+        }if (e.getKeyCode()==KeyEvent.VK_LEFT){
+            px--;
+        }if (e.getKeyCode()==KeyEvent.VK_RIGHT){
+            px++;
+        }
+        if (e.getKeyCode()==KeyEvent.VK_SPACE){
+            createVecs(1);
+        }
     }
+
     public void keyReleased(KeyEvent e) {
 
     }
     public void keyTyped(KeyEvent e) { }
+
+    int getTile(int x, int y){
+        float sum=0;
+        for (float[] v:vecs){
+            sum+=(float)(Math.sin(v[1]*(x-v[1])+v[2]*(y-v[3]))+1);
+        }
+        sum/=vecs.length;
+
+        return (int)(sum/2f*255*255*255);
+    }
 
     //QUICK METHOD I MADE TO DISPLAY A COORDINATE GRID
     public void paintCoordGrid(Graphics gfx){
